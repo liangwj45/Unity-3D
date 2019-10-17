2027b23469e39c203464adf6b0b33115fdf767ea
梁文杰 <liangwj45@mail2.sysu.edu.cn>
2019-10-17 18:53:59 +0800

    Animation

---
+++ SwitchAnimation/Assets/Main.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Main :MonoBehaviour {
+    public bool pressed = false;
+    void Start() {
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log("点击鼠标左键");
+            pressed = !pressed;
+            GetComponent<Animator>().SetBool("pressed", pressed);
+        }
+    }
+}
